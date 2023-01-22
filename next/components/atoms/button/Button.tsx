a8d568f40f36aeca57eebc412e3c8e8fc184225c
Chris Runo <chris.runo@gmail.com>
2023-01-22 10:21:15 -0500

    Add styling to button component.

--- next/components/atoms/button/Button.tsx
+++ next/components/atoms/button/Button.tsx
@@ -19,13 +19,15 @@ export const Button = ({
   ...props
 }: ButtonProps) => {
   return (
-    <a href={link} {...props}>
-      {icon ? (
-        <Icon {...icon}/>
-      ) : (
-        <></>
-      )}
-      {label}
-    </a>
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+      <a href={link} {...props}>
+        {icon ? (
+          <Icon {...icon}/>
+        ) : (
+          <></>
+        )}
+        {label}
+      </a>
+    </button>
   );
 };
