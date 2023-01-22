15c18e5422c60a69ca90f7ff7def5b4afd7272c6
Chris Runo <chris.runo@gmail.com>
2023-01-22 11:24:08 -0500

    Add secondary button story.

--- next/components/atoms/button/Button.stories.ts
+++ next/components/atoms/button/Button.stories.ts
@@ -28,3 +28,11 @@ export const PrimaryButtonIcon: Story = {
     classes: 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded'
   },
 };
+
+export const SecondaryButton: Story = {
+  args: {
+    link: '#',
+    label: 'Button',
+    classes: 'bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'
+  },
+};
