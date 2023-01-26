2c0b1a30cd04dba52d2d691f4c068699a8adc14a
Chris Runo <chris@ifsight.com>
2023-01-26 11:01:39 -0500

    Update index.tsx with Card Group component.

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -1,10 +1,12 @@
 import Head from "next/head"
 import { GetStaticPropsResult } from "next"
 import { DrupalNode } from "next-drupal"
-
 import { drupal } from "lib/drupal"
+import {absoluteUrl} from "../lib/utils";
+
 import { Layout } from "components/layout"
 import { NodeArticleTeaser } from "components/templates/node/article/node--article--teaser"
+import { CardGrid } from "components/organisms/card-grid/CardGrid";
 
 export async function getStaticProps(
   context
@@ -33,7 +35,22 @@ interface IndexPageProps {
   nodes: DrupalNode[]
 }
 
+const cards = [];
+
 export default function IndexPage({ nodes }: IndexPageProps) {
+  if (nodes.length > 0) {
+    nodes.forEach((node) => {
+      cards.push({
+        title: node.title,
+        imagePath: absoluteUrl(node.field_image.uri.url),
+        imageAlt: node.field_image.resourceIdObjMeta.alt,
+        description: "Testing 123...",
+        link: node.path.alias,
+        tags: ""
+      });
+    });
+  }
+
   return (
     <Layout>
       <Head>
@@ -46,12 +63,7 @@ export default function IndexPage({ nodes }: IndexPageProps) {
       <div>
         <h1 className="mb-10 text-6xl font-black">Latest Articles.</h1>
         {nodes?.length ? (
-          nodes.map((node) => (
-            <div key={node.id}>
-              <NodeArticleTeaser node={node} />
-              <hr className="my-20" />
-            </div>
-          ))
+          <CardGrid columns="3" cards={cards}/>
         ) : (
           <p className="py-4">No nodes found</p>
         )}
