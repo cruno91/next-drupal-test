e754a533a9f73a9d954c1bb731f3c7bbc80cef09
Chris Runo <chris@ifsight.com>
2023-01-26 10:20:25 -0500

    Move node--article--teaser into templates directory and update it to us the Card component.

---
+++ next/components/templates/node/node--article--teaser.tsx
@@ -0,0 +1,20 @@
+import { DrupalNode } from "next-drupal"
+import { absoluteUrl, formatDate } from "lib/utils"
+
+import { Card } from "../../molecules/cards/card/Card";
+
+interface NodeArticleTeaserProps {
+  node: DrupalNode
+}
+
+export function NodeArticleTeaser({ node, ...props }: NodeArticleTeaserProps) {
+  return (
+    <Card
+      title={node.title}
+      imagePath={absoluteUrl(node.field_image.uri.url)}
+      imageAlt={node.field_image.resourceIdObjMeta.alt}
+      description="Testing 123..."
+      link={node.path.alias} tags=""
+    />
+  )
+}
--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -4,7 +4,7 @@ import { DrupalNode } from "next-drupal"
 
 import { drupal } from "lib/drupal"
 import { Layout } from "components/layout"
-import { NodeArticleTeaser } from "components/node--article--teaser"
+import { NodeArticleTeaser } from "components/templates/node/node--article--teaser"
 
 interface IndexPageProps {
   nodes: DrupalNode[]
